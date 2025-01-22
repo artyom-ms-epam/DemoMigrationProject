d97c5be8e085cfc56c21eb6b72d64f1f4dd74910
artyom-ms-epam <artem_moskvin@epam.com>
2025-01-22 16:30:36 +0400

    Update test/test.cs

--- test/test.cs
+++ test/test.cs
@@ -19,6 +19,84 @@ namespace TestProject
             _context = context;
         }
 
+        [Given(@"I type name '([^']*)' in the input field")]
+        public async Task GivenITypeNameInTheInputField(string name)
+        {
+            await _page.FillAsync("#developer-name", name);
+        }
+
+        [Then(@"the input field should contain '([^']*)'")]
+        public async Task ThenTheInputFieldShouldContain(string expectedName)
+        {
+            var value = await _page.InputValueAsync("#developer-name");
+            value.Should().Be(expectedName);
+        }
+
+        [When(@"I click on '([^']*)' checkbox")]
+        public async Task WhenIClickOnCheckbox(string checkboxId)
+        {
+            await _page.CheckAsync($"#{checkboxId}");
+        }
+
+        [Then(@"the '([^']*)' checkbox should be checked")]
+        public async Task ThenTheCheckboxShouldBeChecked(string checkboxId)
+        {
+            var isChecked = await _page.IsCheckedAsync($"#{checkboxId}");
+            isChecked.Should().BeTrue();
+        }
+
+        [When(@"I move the slider to '([^']*)'")]
+        public async Task WhenIMoveTheSliderTo(string value)
+        {
+            await _page.FillAsync("#slider", value);
+        }
+
+        [Then(@"the slider should have value '([^']*)'")]
+        public async Task ThenTheSliderShouldHaveValue(string expectedValue)
+        {
+            var value = await _page.InputValueAsync("#slider");
+            value.Should().Be(expectedValue);
+        }
+
+        [When(@"I select '([^']*)' from the dropdown")]
+        public async Task WhenISelectFromTheDropdown(string option)
+        {
+            await _page.SelectOptionAsync("#preferred-interface", new[] { option });
+        }
+
+        [Then(@"the dropdown should have '([^']*)' selected")]
+        public async Task ThenTheDropdownShouldHaveSelected(string expectedOption)
+        {
+            var selectedOption = await _page.InputValueAsync("#preferred-interface");
+            selectedOption.Should().Be(expectedOption);
+        }
+
+        [When(@"I fill the comments with '([^']*)'")]
+        public async Task WhenIFillTheCommentsWith(string comments)
+        {
+            await _page.FillAsync("#comments", comments);
+        }
+
+        [Then(@"the comments should contain '([^']*)'")]
+        public async Task ThenTheCommentsShouldContain(string expectedComments)
+        {
+            var value = await _page.InputValueAsync("#comments");
+            value.Should().Contain(expectedComments);
+        }
+
+        [When(@"I submit the form")]
+        public async Task WhenISubmitTheForm()
+        {
+            await _page.ClickAsync("#submit-button");
+        }
+
+        [Then(@"the results should contain '([^']*)'")]
+        public async Task ThenTheResultsShouldContain(string expectedResults)
+        {
+            var results = await _page.InnerTextAsync("#article-header");
+            results.Should().Contain(expectedResults);
+        }
+
         [BeforeScenario]
         public async Task BeforeScenario()
         {
